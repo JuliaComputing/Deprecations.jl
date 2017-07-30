9a7ee82797b8d1c4ce7c239157a61e083bc19a16
Keno Fischer <kfischer@college.harvard.edu>
2017-07-30 17:16:00 -0400

    Initial work

--- src/Deprecations.jl
+++ src/Deprecations.jl
@@ -1,5 +1,89 @@
 module Deprecations
+    using CSTParser
+    using CSTParser: EXPR
 
-# package code goes here
+    struct Deprecation
+        description::String
+        package::String
+        minsupported::Base.VersionNumber
+        minrequired::Base.VersionNumber
+        maxver::Base.VersionNumber
+    end
+
+    include("astmatching.jl")
+    include("resolutions.jl")
+
+    all_templates = Any[]
+    all_replacements = Any[]
+    deprecation = Any[]
+
+    custom_resolutions = Any[]
+
+    function match(dep::Deprecation, template::String, replacement::String)
+        push!(deprecation, dep)
+        push!(all_templates, template)
+        push!(all_replacements, replacement)
+    end
+
+    function match_macrocall(mod, name, dep::Deprecation, template::String, replacement::String)
+        # TODO: Make sure the macro in the current module is actually the one from `mod`
+        push!(deprecation, dep)
+        push!(all_templates, template)
+        push!(all_replacements, replacement)
+    end
+
+    function match(f::Function, dep::Deprecation, expr_kind)
+        push!(custom_resolutions, (expr_kind, f))
+    end
+
+    include("database.jl")
+
+    struct TextReplacement
+        range::UnitRange{Int}
+        text::String
+    end
+
+    function edit_text(text)
+        replacements = collect(zip(all_templates, all_replacements))
+        parsed_replacementes = map(x->(CSTParser.parse(x[1]),CSTParser.parse(x[2])), replacements)
+        match = CSTParser.parse(text)
+        function find_replacements(x, results)
+            for (i,(t, r)) in enumerate(parsed_replacementes)
+                if typeof(x) == typeof(t)
+                    result = Dict{Any,Any}()
+                    match_parameters(t, x, result)[1] || continue
+                    buf = IOBuffer()
+                    reassemble(buf, r, result, replacements[i][2], text)
+                    push!(results, TextReplacement(x.span, String(take!(buf))))
+                end
+            end
+            for (i,(k, f)) in enumerate(custom_resolutions)
+                if typeof(x) == EXPR{k}
+                    f((x, text, results))
+                end
+            end
+            for arg in x.args
+                find_replacements(arg, results)
+            end
+        end
+        results = TextReplacement[]
+        find_replacements(match, results)
+        sort!(results, by=x->first(x.range))
+        buf = IOBuffer()
+        lastoffset = 0
+        for r in results
+            if lastoffset > first(r.range)
+                # Overlapping replacements. Only apply the first one for now.
+                # We'll re-run this to convergence
+                continue
+            end
+            write(buf, text[1+(lastoffset:first(r.range)-1)])
+            write(buf, r.text)
+            lastoffset = last(r.range)+1
+        end
+        write(buf, text[lastoffset+1:end])
+        String(take!(buf))
+    end
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,62 @@
 using Deprecations
+using Deprecations: edit_text
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@test edit_text("""
+function foobar{T}(x, y::T)
+    println(x, y)
+end
+""") == """
+function foobar(x, y::T) where T
+    println(x, y)
+end
+"""
+
+@test edit_text("""
+module Test
+    function foobar{T}(x, y::T)
+        println(x, y)
+    end
+end
+""") == """
+module Test
+    function foobar(x, y::T) where T
+        println(x, y)
+    end
+end
+"""
+
+@test edit_text("""
+function foobar{S,T}(x::S, y::T)
+    println(x, y)
+end
+""") == """
+function foobar(x::S, y::T) where {S,T}
+    println(x, y)
+end
+"""
+
+edit_text("""
+module Test
+    function foobar{S,T}(x::S, y::T)
+        println(x, y)
+    end
+end
+""") == """
+module Test
+function foobar(x::S, y::T) where {S,T}
+    println(x, y)
+end
+end
+"""
+
+edit_text("f{T}(x::T) = x") == "f(x::T) where {T} = x"
+
+edit_text("""
+if VERSION >= v"0.1.0"
+    do_magic()
+end
+""") == "do_magic()"
+
+edit_text("@compat(Union{Int64,Float64})") == "Union{Int64,Float64}"
+edit_text("@compat Union{Int64,Float64}") == "Union{Int64,Float64}"
