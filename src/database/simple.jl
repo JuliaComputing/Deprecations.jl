96cb286aef850bff0abc64b4a63e9b43fc2179d6
Fredrik Ekre <fredrik.ekre@chalmers.se>
2018-03-09 09:17:27 +0100

    [WIP] Deprecations for functions that got positional args replaced by kwargs (#33)
    * add some Deprecations for functions that got positional args replace by kwargs
    
    * remove parse form keyword fixes
    
    * Update simple.jl

--- src/database/simple.jl
+++ src/database/simple.jl
@@ -369,3 +369,76 @@ end
 @add_rename STDOUT         stdout           v"0.7.0-DEV.4068"
 @add_rename STDERR         stderr           v"0.7.0-DEV.4068"
 @add_rename reprmime       repr             v"0.7.0-DEV.4010"
+
+begin
+    struct KeywordsUnlocked; end
+    register(KeywordsUnlocked, Deprecation(
+        "a bunch of functions got keyword arguments instead of positional arguments",
+        "julia",
+        v"0.7.0-DEV.3526", v"1.0", typemax(VersionNumber)
+    ))
+    match(KeywordsUnlocked,
+        "Timer(\$timeout, \$repeat)",
+        "Timer(\$timeout, interval = \$repeat!)"
+    )
+    match(KeywordsUnlocked,
+        "Timer(\$callback, \$delay, \$repeat)",
+        "Timer(\$callback, \$delay!, interval = \$repeat!)"
+    )
+    match(KeywordsUnlocked,
+        "names(\$m, \$all)",
+        "names(\$m, all = \$all!)"
+    )
+    match(KeywordsUnlocked,
+        "names(\$m, \$all, \$imported)",
+        "names(\$m, all = \$all!, imported = \$imported!)"
+    )
+    match(KeywordsUnlocked,
+        "code_native(\$io, \$f, \$types, \$syntax)",
+        "code_native(\$io, \$f!, \$types!, syntax = \$syntax!)"
+        )
+    match(KeywordsUnlocked,
+        "code_native(\$f, \$types, \$syntax)",
+        "code_native(\$f, \$types!, syntax = \$syntax!)"
+        )
+    match(KeywordsUnlocked,
+        "eachmatch(\$re, \$str, \$overlap)",
+        "eachmatch(\$re, \$str!, overlap = \$overlap!)"
+        )
+    match(KeywordsUnlocked,
+        "matchall(\$re, \$str, \$overlap)",
+        "matchall(\$re, \$str!, overlap = \$overlap!)"
+        )
+    match(KeywordsUnlocked,
+        "chop(\$s, \$head)",
+        "chop(\$s, head = \$head!)"
+        )
+    match(KeywordsUnlocked,
+        "chop(\$s, \$head, \$tail)",
+        "chop(\$s, head = \$head!, tail = \$tail!)"
+        )
+    match(KeywordsUnlocked,
+        "tryparse(\$T, \$s, \$base)",
+        "tryparse(\$T, \$s!, base = \$base!)"
+        )
+    match(KeywordsUnlocked,
+        "mkdir(\$path, \$mode)",
+        "mkdir(\$path, mode = \$mode!)"
+        )
+    match(KeywordsUnlocked,
+        "mkpath(\$path, \$mode)",
+        "mkpath(\$path, mode = \$mode!)"
+        )
+    match(KeywordsUnlocked,
+        "countlines(\$x, \$eol)",
+        "countlines(\$x, eol = \$eol!)"
+        )
+    match(KeywordsUnlocked,
+        "PipeBuffer(\$data, \$maxsize)",
+        "PipeBuffer(\$data, maxsize = \$maxsize!)"
+        )
+    match(KeywordsUnlocked,
+        "unsafe_wrap(\$T, \$pointer, \$dims, \$own)",
+        "unsafe_wrap(\$T, \$pointer!, \$dims!, own = \$own!)"
+        )
+end
